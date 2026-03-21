31ce134d81edf2f4f9d7e293029e095ea522fb4c
Diego <2005diegomez@gmail.com>
2026-03-20 20:32:05 -0600

    Diego sprint1 v2

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,1 +1,13 @@
- 
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL!);
+        console.log("Conectado a MongoDB");
+    } catch (error) {
+        console.error("No se pudo conectar a MongoDB:", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,20 @@
 import Express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+
+dotenv.config(); 
 
 const app = Express();
+const PORT = process.env.PORT || 3000;
+
+app.use(Express.json());
+
+connectDB();
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello World");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
