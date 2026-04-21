b67b128422256ce779da5a34e4f567c0bb3a4857
juanpabloglez25 <juan.gonzalezm@iteso.mx>
2026-04-21 11:53:30 -0600

    chore (server): actualización de configuración de rutas para el dashboard

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -6,6 +6,7 @@ import categoryRouter from './category.routes'
 import expenseRouter from './expense.routes'
 import incomeRouter from './income.routes'
 import savingGoalRouter from './savingGoal.routes'
+import dashboardRouter from "./dashboard.routes";
 
 const router = Router();
 
@@ -16,5 +17,6 @@ router.use('/incomes', incomeRouter);
 router.use('/categories', categoryRouter);
 router.use('/budgets', budgetRouter);
 router.use('/saving-goals', savingGoalRouter);
+router.use("/dashboard", dashboardRouter);
 
 export default router;
