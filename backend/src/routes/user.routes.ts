09ca3315002b8f7874a4e8932f430ab9e4e2d110
juanpabloglez25 <juan.gonzalezm@iteso.mx>
2026-04-21 10:53:30 -0600

    feat(users): restringí la actualización y elminación de usuarios a ADMIN

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,4 +1,3 @@
-console.log("🔥 user.routes cargado");
 import { Router } from "express";
 import {
   getUsers,
@@ -7,13 +6,31 @@ import {
   deleteUser,
 } from "../controllers/user.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
+import { authorizeRoles } from "../middlewares/role.middleware";
 
 const router = Router();
 
-//protección de las rutas con authmiddleware
+// DEBUG (quitalo después)
+console.log("user.routes cargado");
+
+// Rutas protegidas (solo autenticación)
 router.get("/", authMiddleware, getUsers);
 router.get("/:id", authMiddleware, getUserById);
-router.put("/:id", authMiddleware, updateUser);
-router.delete("/:id", authMiddleware, deleteUser);
+
+// Solo ADMIN puede actualizar
+router.put(
+  "/:id",
+  authMiddleware,
+  authorizeRoles("ADMIN"),
+  updateUser
+);
+
+// Solo ADMIN puede eliminar
+router.delete(
+  "/:id",
+  authMiddleware,
+  authorizeRoles("ADMIN"),
+  deleteUser
+);
 
 export default router;
