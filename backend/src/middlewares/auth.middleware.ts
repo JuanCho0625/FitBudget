538ccdf1a1040eab48e317dd857d5e43d5752200
juanpabloglez25 <juan.gonzalezm@iteso.mx>
2026-04-21 10:08:38 -0600

    feat (auth): se agregó JWT middleware para la protección de la ruta

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,7 +1,12 @@
+console.log(" Middleware ejecutándose");
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 export interface AuthRequest extends Request {
+  user?: {
+    id: string;
+    role: string;
+  };
   userId?: string;
 }
 
@@ -20,11 +25,14 @@ export const authMiddleware = (
 
     const token = authHeader.split(" ")[1];
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
-      id: string;
-    };
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as { id: string; role: string };
 
+    req.user = decoded;
     req.userId = decoded.id;
+
     next();
   } catch (error) {
     res.status(401).json({ message: "Token inválido o expirado" });
