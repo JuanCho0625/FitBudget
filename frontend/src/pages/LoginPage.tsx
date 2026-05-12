6ecf1a6e3044586969db7ef9a64e7959ffeb3390
juanpabloglez25 <juan.gonzalezm@iteso.mx>
2026-05-12 08:24:18 -0600

    Create controlled login form

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+
 function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+
+        console.log("Email:", email);
+        console.log("Password:", password);
+    };
+
     return (
         <div>
-            <h1>Login FitBudget</h1>
+            <h1>FitBudget Login</h1>
+
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email</label>
+
+                    <input
+                        type="email"
+                        placeholder="Enter your email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <label>Password</label>
+
+                    <input
+                        type="password"
+                        placeholder="Enter your password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                </div>
+
+                <button type="submit">Login</button>
+            </form>
         </div>
     );
 }
